e99ef3180741bba034df86a251a99e622c4a588a
K Lokeshwaran <lokibg2@gmail.com>
2016-08-13 00:41:52 +0530

    Templates!  Dejavu Angular :P

--- music/views.py
+++ music/views.py
@@ -1,16 +1,12 @@
 from django.http import HttpResponse, Http404
+from django.shortcuts import render
 from .models import Album
+
 
 def index(request):
     albums = Album.objects.all()
-
-    html = '<h1 style="text-align:center"> This is Music!!!! :) </h1>'
-    html += '<hr><br><ul>'
-
-    for album in albums:
-        url = '/music/' + str(album.id)
-        html += '<li><a href = "' + url + '">' + album.albumTitle + '</a>'
-    return HttpResponse(html)
+    context = {'albums': albums}
+    return render(request, 'music/index.html', context)
 
 
 def error(request):
