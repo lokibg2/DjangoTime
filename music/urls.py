917d2ca8b67192486f3c46332e3c66d9105c16d6
K Lokeshwaran <lokibg2@gmail.com>
2016-08-13 00:16:10 +0530

    Views -  The Python way! ;)

--- music/urls.py
+++ music/urls.py
@@ -3,7 +3,13 @@ from . import views
 
 
 urlpatterns = [
+    # /music/
     url(r'^$', views.index, name='index'),
+
+    # /music/id/
+    url(r'^(?P<albumId>[0-9]*)[/]?$', views.details, name='details'),
+
+    # Invalid
     url(r'.*', views.error, name='error')
 
 ]
--- music/views.py
+++ music/views.py
@@ -1,10 +1,21 @@
 from django.http import HttpResponse, Http404
-
+from .models import Album
 
 def index(request):
-    return HttpResponse('<h1> Hey Buddy! :) </h1>')
+    albums = Album.objects.all()
+
+    html = '<h1 style="text-align:center"> This is Music!!!! :) </h1>'
+    html += '<hr><br><ul>'
+
+    for album in albums:
+        url = '/music/' + str(album.id)
+        html += '<li><a href = "' + url + '">' + album.albumTitle + '</a>'
+    return HttpResponse(html)
 
 
 def error(request):
-    return HttpResponse('Error! Wrong Page!!')
+    return HttpResponse('<h1 style="text-align:center">Error! Wrong Page!!</h1>')
 
+
+def details(request, albumId):
+    return HttpResponse('<h2 style="text-align:center"> You\'re at Album No : ' + albumId + '</h2>')
