23a572fd1b1f516fb1e482516744b754b9979446
K Lokeshwaran <lokibg2@gmail.com>
2016-08-12 23:23:15 +0530

    Django scores with models!!!

--- music/models.py
+++ music/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Album(models.Model):
+    albumTitle = models.CharField(max_length=250)
+    artist = models.CharField(max_length=500)
+    genre = models.CharField(max_length=50)
+    albumArt = models.CharField(max_length=500)
+
+
+class Song(models.Model):
+    album = models.ForeignKey(Album, on_delete=models.CASCADE)
+    songTitle = models.CharField(max_length=300)
+    fileType = models.CharField(max_length=10)
--- test/urls.py
+++ test/urls.py
@@ -15,7 +15,6 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
-from django.http import HttpResponse
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
